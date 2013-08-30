42ec96dc16c9467c64567ca5328298240f1b28e5
RMed <rafamedgar@gmail.com>
2013-08-30 14:01:22 +0200

    Added setup script (only source and translation building at the moment), added build tools directory, modified README file

---
+++ setup.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# This file is part of TextVentures - https://github.com/RMed/textventures
+#
+# Copyright (C) 2013 Rafael Medina García <rafamedgar@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from distutils.command.build import build as _build
+from distutils.core import setup
+from tools import build_trans
+
+# Override the default build function to include the compilation of the
+# translation files
+class build(_build):
+    # Add custom commands
+    sub_commands = _build.sub_commands + [('build_locale', None)]
+
+    def run(self):
+        _build.run(self)
+
+setup(
+    # Standard information
+    name='TextVentures',
+    version='0.2.0',
+    description='A simple text-based adventure system',
+    author='Rafael Medina García (RMed)',
+    author_email='rafamedgar@gmail.com',
+    url='http://github.com/RMed/textventures',
+    license = 'GPLv2',
+
+    # Extra files for distribution
+    data_files=[('', ['LICENSE', 'AUTHORS'])],
+    # Packages to build
+    packages = ['textventures', 'textventures.parser', 
+            'textventures.instances'],
+    # Set package source root location
+    package_dir = {'': 'src'},
+    # Include main module
+    py_modules = ['play_textventures'],
+
+    # Custom commands
+    cmdclass = {
+        'build': build,
+        'build_locale': build_trans.BuildTranslations
+    },
+
+    # Build options
+    options = {
+        # Platform independent, use build directory directly
+        'build': { 'build_lib': 'build' } 
+    })
+
